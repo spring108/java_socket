73c7ea527d8fd76b5bfb5515187f9c21c59e7bf7
Игорь Скрипников <54932391+spring108@users.noreply.github.com>
2023-10-09 13:17:12 +0300

    Update socket.bat

--- socket.bat
+++ socket.bat
@@ -1,22 +1,3 @@
 java socket.java
 pause
 
-URL = http://host:port/SM/9/rest/SERVICE_MAME/SD08888888/attachments
-Method = POST
-
-Закладка Auth = Basic
-
-Закладка Headers:
-Content-Type: application/octet-stream
-Content-Disposition: attachment; filename*=UTF-8''qq.txt
-Connection: close
-Где:
-Content-Disposition: attachment; filename*=UTF-8''%D4D4%EE
-Content-disposition: attachment;filename*=UTF-8''%D1%82%D0%B5%D1%81%D1%82.txt
-URL-кодировка в кодах символов по правилам RFC6266
-
-Закладка Attachments:
-Добавляем файл и в его свойствах ставим Content-Type = application/octet-stream
-
-В окне:
-Media-type: application/octet-stream
